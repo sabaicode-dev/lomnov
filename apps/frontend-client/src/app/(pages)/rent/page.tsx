38e5ce0e5f319a415a4d374da855710df4a07995
unknown <seyhaoeurn920@gmail.com>
2024-06-08 18:05:08 +0700

    update

--- apps/frontend-client/src/app/(pages)/rent/page.tsx
+++ apps/frontend-client/src/app/(pages)/rent/page.tsx
@@ -2,7 +2,7 @@
 function page() {
   return (
     <div>
-
+rent
     </div>
   )
 }
--- apps/frontend-client/src/app/page.tsx
+++ apps/frontend-client/src/app/page.tsx
@@ -1,19 +1,9 @@
-
-import { Button } from 'ms-ui-components'
-import logger, { loggerBeautifulObject } from '@utils/logger';
+import HeroSection from "@/components/organisms/hero-section/HeroSection";
 
 export default function Home() {
-  logger('hello')
-  loggerBeautifulObject({
-    foo: 'bar',
-    'arr': [1, 2, 3],
-    nested: {
-      hello: "world"
-    }
-  })
   return (
-    <main className="flex  flex-col  justify-between p-24">
-
+    <main className="">
+      <HeroSection/>
     </main>
   );
 }
