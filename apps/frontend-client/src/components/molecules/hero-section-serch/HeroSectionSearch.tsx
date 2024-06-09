cf31dfff28b743f9ad3bce8cd61a0df40b1d025a
unknown <seyhaoeurn920@gmail.com>
2024-06-09 14:48:35 +0700

    hero section search

---
+++ apps/frontend-client/src/components/molecules/hero-section-serch/HeroSectionSearch.tsx
@@ -0,0 +1,68 @@
+"use client"
+import React from 'react'
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { IconSearch , IconLocation} from '@/icons';
+
+// ================================
+
+function HeroSectionSearch() {
+  const [search, setSearch] = useState("");
+  const [showResults, setShowResults] = useState(false);
+  const searchRef = useRef<HTMLDivElement>(null);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    setShowResults(e.target.value !== "");
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (searchRef.current && !searchRef.current.contains(event.target as Node)) {
+      setShowResults(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className=" w-full absolute   left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] z-20 flex flex-col lg:gap-10 gap-3 justify-center items-center ">
+        <h1 className="text-[25px] lg:text-[40px] text-white uppercase font-[600] ">
+          Real Estate Platform
+        </h1>
+        <div ref={searchRef} className="w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%]  h-[45px] md:h-[50px]  bg-white rounded-md overflow-hidden flex flex-row items-center gap-5 px-5 ">
+          <input
+            type="text"
+            className="h-full outline-none  py-4 w-[90%]"
+            placeholder="Enter an address..."
+            onChange={(e) => handleSearch(e)}
+          />
+
+          <div className="border-[0.8px] border-solid  border-black h-[20px] "></div>
+          <IconLocation props="text-blue-500 text-[18px]" />
+        </div>
+
+        {
+          showResults &&
+          <div ref={searchRef} className=" w-[80%] md:w-[60%] xl:w-[40%] h-fit bg-[#fffffff2] absolute top-[110%] border-[0.8px] border-gray-100 rounded-md p-5 shadow-slate-100 shadow-md max-h-[300px] overflow-auto">
+            <ul className=" flex flex-col gap-3  md:gap-5 ">
+              <Link href={""} className=" flex flex-row gap-5"> <span className="w-[90%] text-[14px] md:text-[16px]"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, a...</span> <IconSearch props=" text-[20px] text-[gray]" /> </Link>
+              <Link href={""} className=" flex flex-row gap-5"> <span className="w-[90%] text-[14px] md:text-[16px]"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, a...</span> <IconSearch props=" text-[20px] text-[gray]" /> </Link>
+              <Link href={""} className=" flex flex-row gap-5"> <span className="w-[90%] text-[14px] md:text-[16px]"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, a...</span> <IconSearch props=" text-[20px] text-[gray]" /> </Link>
+              <Link href={""} className=" flex flex-row gap-5"> <span className="w-[90%] text-[14px] md:text-[16px]"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, a...</span> <IconSearch props=" text-[20px] text-[gray]" /> </Link>
+              <Link href={""} className=" flex flex-row gap-5"> <span className="w-[90%] text-[14px] md:text-[16px]"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, a...</span> <IconSearch props=" text-[20px] text-[gray]" /> </Link>
+              <Link href={""} className=" flex flex-row gap-5"> <span className="w-[90%] text-[14px] md:text-[16px]"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, a...</span> <IconSearch props=" text-[20px] text-[gray]" /> </Link>
+            </ul>
+          </div>
+        }
+      </div>
+    </>
+  )
+}
+
+export default HeroSectionSearch
