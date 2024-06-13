c796b64da7a7f30692b5ac7ad0a60deae924f8ad
virak <virakson444@gmail.com>
2024-06-13 13:28:50 +0700

    const errors: FormErrors = {}  Initialize Errors Object: Creates an empty object to store validation errors.

--- apps/frontend-client/src/app/(pages)/(auth)/signin/LoginForm.tsx
+++ apps/frontend-client/src/app/(pages)/(auth)/signin/LoginForm.tsx
@@ -25,6 +25,21 @@ const LoginForm: React.FC = () => {
       return { ...prevErrors, [name]: "" };
     });
   };
+
+  const validate = (): FormErrors => {
+    const errors: FormErrors = {};
+    if (!formData.email) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = "Email is invalid";
+    }
+    if (!formData.password) {
+      errors.password = "Password is required";
+    } else if (formData.password.length < 8) {
+      errors.password = "Password must be at least 8 characters long";
+    }
+    return errors;
+  };
 };
 
 export default LoginForm;
