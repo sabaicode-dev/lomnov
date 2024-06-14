9d5ae125920b2f47bf67edffd347f306dde613f4
virak <virakson444@gmail.com>
2024-06-14 13:58:27 +0700

    add footer

--- apps/frontend-client/src/app/(pages)/(auth)/signin/page.tsx
+++ apps/frontend-client/src/app/(pages)/(auth)/signin/page.tsx
@@ -4,6 +4,7 @@ import { Facebook, Google } from "@/icons";
 import Image from "next/image";
 import Link from "next/link";
 import image from "@/images/Login logo.png";
+import Footer from "@/components/organisms/footer/Footer";
 
 const SignInPage = () => {
   return (
@@ -11,7 +12,7 @@ const SignInPage = () => {
       className="container mx-auto p-4 md:py-20"
       style={{ maxWidth: "1300px" }}
     >
-      <div className="flex flex-col sm:flex-row gap-8 md:gap-32 justify-center items-center">
+      <div className="flex flex-col sm:flex-row gap-8 md:gap-32 justify-center items-center mb-10">
         <Image
           src={image}
           alt="Login logo"
@@ -40,6 +41,9 @@ const SignInPage = () => {
             </Link>
           </div>
         </div>
+      </div>
+      <div className=" hidden sm:block">
+        <Footer />
       </div>
     </div>
   );
