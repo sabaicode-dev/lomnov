ca9c01a9b2120e5a8aec3085f4b50876897ee90b
virak <virakson444@gmail.com>
2024-06-16 15:48:29 +0700

    add loginform, inputform, userschema

--- packages/ui-components/src/index.tsx
+++ packages/ui-components/src/index.tsx
@@ -1,3 +1,7 @@
 import Button from "./components/Button";
 
 export { Button };
+
+export { default as LoginForm } from "./components/form/LoginForm";
+export { default as InputField } from "./components/inputfield/InputField";
+export { UserSchema } from "./schemas/userSchema";
