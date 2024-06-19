90e1410dcc1769f4115c27dfa16bd5ecc80b65b1
virak <virakson444@gmail.com>
2024-06-19 15:20:04 +0700

    updated

--- apps/frontend-client/.storybook/preview.ts
+++ apps/frontend-client/.storybook/preview.ts
@@ -1,19 +1,14 @@
 import type { Preview } from "@storybook/react";
-import "../src/app/globals.css"
+import "../src/app/globals.css";
 import "./storybook-styles.css";
-import MyTheme from "./MyTheme";
-
 
 const preview: Preview = {
   parameters: {
-    // controls: {
-    //   matchers: {
-    //     color: /(background|color)$/i,
-    //     date: /Date$/i,
-    //   },
-    // },
-    docs: {
-      theme: MyTheme,
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
     },
   },
 };
