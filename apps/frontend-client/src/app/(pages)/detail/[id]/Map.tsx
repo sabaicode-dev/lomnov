71a83e8a520c8e49821ad2d08dccde07aa4409e7
virak <virakson444@gmail.com>
2024-08-05 21:59:36 +0700

    update

---
+++ apps/frontend-client/src/app/(pages)/detail/[id]/Map.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Map = () => {
+  return (
+    <div className="max-w-[1300px] h-[400px] mx-auto mt-[50px] px-[10px]">
+      <div className="w-full h-full">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.312641463294!2d104.92257917580335!3d11.601045188602363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310951f3148296db%3A0x5b289f3f5cef444!2sSabaiCode!5e0!3m2!1skm!2skh!4v1722867946658!5m2!1skm!2skh"
+          width="100%"
+          height="100%"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default Map;
