b13eb0f7ae74ab72e45de0904172bc0c6645ac77
unknown <seyhaoeurn920@gmail.com>
2024-09-14 16:55:50 +0700

    update

--- apps/backend/auth-service/src/middlewares/cookies.ts
+++ apps/backend/auth-service/src/middlewares/cookies.ts
@@ -1,13 +1,19 @@
-import { CookieOptions, Response } from "express";
+import { CookieOptions, Response } from 'express';
 
-function setCookie(response: Response, name: string, value: string, options: CookieOptions = {}) {
+function setCookie(
+  response: Response,
+  name: string,
+  value: string,
+  options: CookieOptions = {}
+) {
   const defaultOptions: CookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // Secure in production only
-    sameSite: 'lax',
-    maxAge: 3600 * 5,  // 5 minutes expiration
-    ...options,       // Override defaults with provided options
+    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+    sameSite: 'lax', // Default to lax same-site policy
+    maxAge: 5 * 60 * 1000,  // Default 5 minutes expiration
+    ...options, // Allow overriding defaults
   };
+
   response.cookie(name, value, defaultOptions);
 }
 
