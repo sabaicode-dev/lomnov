93b8d68005477138aa6d0152cec9cb5894397d10
unknown <seyhaoeurn920@gmail.com>
2024-09-21 11:18:31 +0700

    update

--- apps/backend/api-gateway/build-script.js
+++ apps/backend/api-gateway/build-script.js
@@ -46,6 +46,17 @@ esbuild.build({
 }).then(() => {
 
   // fs.copySync(path.resolve(__dirname, 'src/configs/.env.production'), path.resolve(__dirname, 'build/configs/.env.production'));
+    // (1) Solve: Copy swagger.json after successful build
+    fs.copySync(path.resolve(__dirname, 'src/docs/swagger.json'), path.resolve(__dirname, 'build/docs/swagger.json'));
+    console.log('Swagger JSON copied successfully!');
+
+    // Copy package.json after ensuring the build was successful
+    fs.copySync(path.resolve(__dirname, 'package.json'), path.resolve(__dirname, 'build/package.json'));
+    console.log('Package.json copied successfully!');
+
+    // Copy ecosystem.config.js after ensuring the build was successful
+    fs.copySync(path.resolve(__dirname, 'ecosystem.config.js'), path.resolve(__dirname, 'build/ecosystem.config.js'));
+    console.log('Ecosystem Config copied successfully!');
 
 }).catch(error => {
   console.error('Build failed:', error);
