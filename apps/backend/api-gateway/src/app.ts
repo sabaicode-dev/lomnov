357be4813e481c129bd778186c894a2789b6629a
unknown <seyhaoeurn920@gmail.com>
2024-09-21 09:42:53 +0700

    update

--- apps/backend/api-gateway/src/app.ts
+++ apps/backend/api-gateway/src/app.ts
@@ -11,7 +11,7 @@ import cors from "cors"
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:3000','http://localhost:4000' ], // Your frontend URL
+  origin: 'http://localhost:3000', // Your frontend URL
   credentials: true, // Allow credentials (cookies)
 }));
 // ===============================
