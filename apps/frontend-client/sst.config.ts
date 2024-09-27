f039621b0895c3b8b735b1132d70e15f01064f00
unknown <seyhaoeurn920@gmail.com>
2024-09-27 15:06:32 +0700

    update: fixed error when build

--- apps/frontend-client/sst.config.ts
+++ apps/frontend-client/sst.config.ts
@@ -1,16 +1,42 @@
+// import { SSTConfig } from "sst";
+// import { NextjsSite } from "sst/constructs";
+
+// export default {
+//   config(_input) {
+//     return {
+//       name: "lomnov",
+//       region: "us-east-1",
+//     };
+//   },
+//   stacks(app) {
+//     app.stack(function Site({ stack }) {
+//       const site = new NextjsSite(stack, "site");
+
+//       stack.addOutputs({
+//         SiteUrl: site.url,
+//       });
+//     });
+//   },
+// } satisfies SSTConfig;
+
 import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
 export default {
   config(_input) {
     return {
-      name: "lomnov",
+      name: "U-Plan",
       region: "us-east-1",
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+          environment: {
+            NEXT_PUBLIC_BASE_URL_AUTH: process.env.NEXT_PUBLIC_BASE_URL_AUTH || '',
+            // NEXT_PUBLIC_VAPID_KEY: process.env.NEXT_PUBLIC_VAPID_KEY || ''
+          }
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
@@ -18,3 +44,4 @@ export default {
     });
   },
 } satisfies SSTConfig;
+
