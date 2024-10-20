8920ef2e1c807f6a70c4ab600ca72e473f35bd38
Rong <bunbunrong993@gmail.com>
2024-10-20 15:23:25 +0700

    test cm

--- apps/frontend-client/src/app/(pages)/(auth)/signin/page.tsx
+++ apps/frontend-client/src/app/(pages)/(auth)/signin/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LoginForm from "../../../../components/organisms/auth/form-login/LoginForm";
-import { Facebook, Google } from "@/icons";
+// import { Facebook, Google } from "@/icons";
 import Image from "next/image";
 import banner from "@/images/banner.png";
 
