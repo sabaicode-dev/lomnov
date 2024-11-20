9bcb2a63049aea5cdb3f541f6f1dac7acb10dc5d
Rong <bunbunrong993@gmail.com>
2024-11-20 14:10:39 +0700

    move file

---
+++ apps/frontend-client/src/components/atoms/property-type-category/PropertyTypeCategory.tsx
@@ -0,0 +1,25 @@
+import { RealEstateItem } from '@/context/user';
+import { Home2 } from '@/icons'
+import React from 'react'
+interface PropertyTypeCategoryProps {
+    property: {
+        category: { content: string; language: string }[]
+    };
+}
+const PropertyTypeCategory = ({ property }:{property:RealEstateItem}) => {
+    return (
+        <div className=" text-center  rounded-[10px]">
+            <div className="flex items-center justify-center mb-[7px]">
+                <Home2 props="text-olive-green w-[20px] h-[20px] md:w-[30px] md:h-[30px] xl:w-[30px] xl:h-[30px]" />
+            </div>
+            <div className="font-helvetica uppercase text-helvetica-caption md:text-helvetica-small lg:text-helvetica-paragraph  font-bold text-olive-green">
+                PROPERTY TYPE
+            </div>
+            <div className="font-helvetica text-helvetica-paragraph2 font-black lg:text-helvetica-h3 uppercase text-charcoal">
+                {property.category[0]?.content}
+            </div>
+        </div>
+    )
+}
+
+export default PropertyTypeCategory
