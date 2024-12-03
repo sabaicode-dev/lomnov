b4405dc1f23ea61a77cc2affe45049734df629a2
Sokphol <sokphol123@gmail.com>
2024-12-03 08:55:59 +0700

    cart new

--- apps/frontend-client/src/components/molecules/properties-action/PropertyActions.tsx
+++ apps/frontend-client/src/components/molecules/properties-action/PropertyActions.tsx
@@ -3,41 +3,26 @@ import Link from "next/link";
 import React from "react";
 
 interface PropertyActionsProps {
-  selectedProperties: string[];
+ 
   onPost: () => void;
-  onUpdate: () => void;
-  onDelete: () => void;
+ 
 }
 
 const PropertyActions: React.FC<PropertyActionsProps> = ({
-  selectedProperties,
+  
   onPost,
-  onUpdate,
-  onDelete,
+
 }) => {
   return (
     <div className="flex justify-end space-x-[10px] items-center font-helvetica text-helvetica-paragraph text-charcoal font-bold">
       <Link
-        className="py-[5px] px-[25px] rounded-[5px] bg-olive-green text-white   "
+        className="w-[200px] h-[38px] text-[16px] rounded-[5px] bg-olive-green text-white px-[10px] py-[8px] text-center   "
         href={"/post-property"}
-        // onClick={onPost}
+        onClick={onPost}
       >
-        Post
+        + New Property
       </Link>
-      <button
-        className="py-[5px] px-[25px] rounded-[5px] bg-[#BCBCB3]"
-        onClick={onUpdate}
-        disabled={selectedProperties.length !== 1}
-      >
-        Update
-      </button>
-      <button
-        className="py-[5px] px-[25px] rounded-[5px] bg-[#E5D2B0] border border-pale-gray"
-        onClick={onDelete}
-        disabled={selectedProperties.length === 0}
-      >
-        Delete
-      </button>
+     
     </div>
   );
 };
