565085933f95619eb63381a30521b648bb9be83b
Rong <bunbunrong993@gmail.com>
2024-12-04 15:06:09 +0700

    build to production

--- apps/frontend-client/src/components/organisms/user-posted-properties/UserPostedProperties.tsx
+++ apps/frontend-client/src/components/organisms/user-posted-properties/UserPostedProperties.tsx
@@ -12,7 +12,9 @@ export default async function UserPostedProperties({ property }: { property: Rea
       <article className="grid mt-5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 sm:gap-5 md:gap-5 lg:gap-5 xl:gap-5 2xl:gap-5">
         {property.length > 0 ? (
           property.map((properties) => (
-            <ItemCard key={properties._id} item={properties} />
+            <ItemCard key={properties._id} item={properties} toggleCompare={function (item: RealEstateItem): void {
+              throw new Error("Function not implemented.");
+            } } />
           ))
         ) : (
           <p>No properties found.</p>
