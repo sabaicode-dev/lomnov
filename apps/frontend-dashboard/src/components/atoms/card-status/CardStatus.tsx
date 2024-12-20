5a508c0be42079c870242d8692e213439ff65ac3
Rong <bunbunrong993@gmail.com>
2024-12-20 15:57:50 +0700

    mak

--- apps/frontend-dashboard/src/components/atoms/card-status/CardStatus.tsx
+++ apps/frontend-dashboard/src/components/atoms/card-status/CardStatus.tsx
@@ -4,6 +4,6 @@ type CardStatusProps = {
 }
 export default function CardStatus({status} : CardStatusProps) {
     return (
-        <div className='w-[200px] flex justify-start' >{status === "Active" ? (<p className=" bg-Negative/20  text-Negative px-[4px] border-2 border-Negative/20 rounded-[6px]">{status}</p>) : (<p className="bg-Positive/20 text-Positive px-[4px] border-2 border-Positive/20 rounded-[6px]">{status}</p>)}</div>
+        <div className='w-[200px] flex justify-start'>{status === true ? (<p className=' bg-Positive/20   text-Positive px-[4px]  rounded-[6px] border-2 border-Positive/20'>Active</p>):(<p className=' bg-Negative/20   text-Negative px-[4px]  rounded-[6px] border-2 border-Negative/20'>Inactive</p>)}</div>
     )
 }
