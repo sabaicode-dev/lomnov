047ec610309aa1ba1a4b070ceac8cfb2b200d7fb
Rong <bunbunrong993@gmail.com>
2025-01-09 09:02:00 +0700

    fix cors origin

--- apps/backend/api-gateway/src/utils/corsOptions.ts
+++ apps/backend/api-gateway/src/utils/corsOptions.ts
@@ -2,8 +2,8 @@ import configs from "../config";
 
 const corsOptions = {
   origin: [
-    configs.clientUrl, // Ensure this is a valid URL, e.g., "http://localhost:3000"
-    configs.CLIENT_DASHBOARD_URL // Explicitly specify the full URL
+    configs.clientUrl,
+    configs.CLIENT_DASHBOARD_URL
   ],
   credentials: true, // Allow credentials (cookies, etc.)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed HTTP methods
